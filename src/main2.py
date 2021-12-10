d8a3c3d45315ad563c6a666555bac8202c1a087a
cescconkarin <95910121+cesconkarin@users.noreply.github.com>
2021-12-10 11:22:33 +0000

    aggiunte varie per provare

--- src/main2.py
+++ src/main2.py
@@ -13,3 +13,44 @@ print(d)
 #<type 'int'>
 #if(d):
 #print(True)
+
+var1 = 'Hello World!'
+var2 = "Python Programming"
+print ("var1[0]: ", var1[0])
+print ("var2[1:5]: ", var2[1:5])
+
+
+a = "Hello"
+c= a+a+a                        #concatenazione
+print(c)
+
+a = "He"+"l"*2+"o World"        #concatenazione multipla
+print(a)
+
+a = "What’s your name?" # Ok if I create the string with double quotes
+b = "What\’s your name?"  # Ok if you escape the single quote character
+print (a)
+print(b)
+
+a = r"Hello \t World"
+#Raw string
+print(a)
+ 
+name = "karin"
+my_string = "ciao %s" % name # Append and Insert
+my_string = "ciao %s, come stai? %s" % (name, 'tutto bene, grazie') # Insert multiple values
+print(my_string)
+
+person = {"name": "karin", "age": 21}
+print(f"{person['name']} is {person['age']} years old.")
+
+s="Ciao a tuttiiii"
+d=s.replace('o','i',1)
+print(d)
+
+s = "Hello"
+d=s.center(10,'.')
+print(d)
+
+d=s.upper()
+print(d)
