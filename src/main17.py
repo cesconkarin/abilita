2048603ac2bf71f21710c439631534d78439653e
cescconkarin <95910121+cesconkarin@users.noreply.github.com>
2021-12-17 11:03:30 +0000

    new 17.2

--- src/main17.py
+++ src/main17.py
@@ -1,3 +1,9 @@
+import sys
+import math
+import numpy as np
+import time 
+
+
 print("Esercizi list_eeeeee")
 l=[]
 import math
@@ -109,3 +115,24 @@ else:
   print("The while lop is over")
 print("done")
 
+
+print("Indovinello per Dementi")
+print("Benvenuta/o all'indovinello per VERI dementi")
+print("Rispondi alla seguente domanda:")
+print("Asciuga bagnandosi. Cos'è? (quit se non ti piace questo gioco)")
+test=True
+while test:
+ guess=input()
+ if guess=="asciugamano":
+  print("e la risposta esatta è...")
+  print("*suspence*")
+  guess=guess+"!"
+  for x in guess:
+   print(x) 
+  test=False
+ elif guess=="quit":
+  test=False
+ else:
+  print("Non ci siamo...")
+else:
+ print("Grazie per aver giocato")
