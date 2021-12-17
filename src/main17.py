21154a9c2169ec0b501cf7629321152d1c288829
cescconkarin <95910121+cesconkarin@users.noreply.github.com>
2021-12-17 10:17:44 +0000

    new main17py

---
+++ src/main17.py
@@ -0,0 +1,19 @@
+print("Esercizi list_eeeeee")
+l=[]
+import math
+k=["Karin","Cescon"]
+
+
+print(k[1])
+m=[0,1,2,3,4]
+print(m[0:3])
+print(m[-4])
+m[0:3]
+print(m)
+
+
+a=range(8)
+b=[int((x*3)+math.pi) for x in a]
+c=[x*3 for x in a]
+print(b[0:6])
+print(c[0:6])
